0cb911134ec822260b10561167970fe88e75c82f
John W. Fail <fail@cenotaph.org>
2013-10-30 13:57:03 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pixelinfra::Application.config.secret_key_base = 'f8fbbc873c9f77ab18ac74067c676cd3deba7f9e6110b3e05669a6e7f22b6c1d6f0c8c0ad233b3c13b62616e58f9a596761c657a889ab413db324da631a257af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pixelinfra::Application.config.session_store :cookie_store, key: '_pixelinfra_session'
