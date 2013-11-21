6bbb095b0ea00bfd417b3b62940e527211bf0d93
John W. Fail <fail@cenotaph.org>
2013-11-21 11:48:21 +0100

    adding French locale

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.default_locale = :en
-I18n.available_locales = [:en, :fi]
-I18n.fallbacks[:en] = [:en, :fi]
-I18n.fallbacks[:fi] = [:fi, :en]
+I18n.available_locales = [:en, :fr, :fi]
+I18n.fallbacks[:en] = [:en, :fr, :fi]
+I18n.fallbacks[:fi] = [:fi, :fr, :en]
 
 Globalize.fallbacks = {:en => [:en, :fi], :fi => [:fi, :en] }
