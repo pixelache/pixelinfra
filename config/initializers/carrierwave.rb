3093b9369a0593f3453be736dba650db0f4e0082
John W. Fail <fail@cenotaph.org>
2013-11-29 21:55:35 +0200

    minor changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|    config.fog_credentials = {
     :aws_access_key_id      => ENV['S3_ACCESS'],       # required
     :aws_secret_access_key  => ENV['S3_SECRET']  ,    # required
     :region                 => 'eu-west-1',
-    :endpoint               => 'https://s3-eu-west-1.amazonaws.com/'
+    :endpoint               => 'https://s3-eu-west-1.amazonaws.com'
   }
   config.fog_directory  = "pixelache"
 
