065f29b5e8eb14338f78ed1159e639964fbe64da
John W. Fail <fail@cenotaph.org>
2014-04-01 17:17:46 +0300

    Public etherpad fixes

--- app/controllers/etherpads_controller.rb
+++ app/controllers/etherpads_controller.rb
@@ -1,7 +1,10 @@
 class EtherpadsController < InheritedResources::Base
   actions :index
-  
+  has_scope :by_festival
+  has_scope :by_project
+  has_scope :by_subsite
+
   def index
-    @etherpads = Etherpad.not_private.order("lower(name)")
+    @etherpads = apply_scopes(Etherpad).not_private.order("lower(etherpads.name)")
   end
 end
