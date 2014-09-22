8dc9697b547d4fa7b8c8fd098ac226b0a8038d16
John W. Fail <fail@cenotaph.org>
2014-09-22 14:05:08 +0300

    new view tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,38 +34,58 @@ module ApplicationHelper
   
   def date_range(from_date, until_date, options = {})
     return I18n.l(from_date.to_date, :format => :long) if until_date.nil?
-      options.symbolize_keys!
-      format = options[:format] || :short
-      separator = options[:separator] || "—"
+    options.symbolize_keys!
+    format = options[:format] || :short
+    separator = options[:separator] || "—"
 
-      if format.to_sym == :short
-        month_names = I18n.t("date.abbr_month_names")
+    if format.to_sym == :short
+      month_names = I18n.t("date.abbr_month_names")
+    else
+      month_names = I18n.t("date.month_names")
+    end
+
+    from_day = from_date.day
+    from_month = month_names[from_date.month]
+    from_year = from_date.year
+    until_day = until_date.day
+
+    if from_date.month == until_date.month
+      if from_date.day == until_date.day
+        I18n.l(from_date.to_date, :format => :long)
       else
-        month_names = I18n.t("date.month_names")
+        I18n.t("date_range.#{format}.same_month", from_day: from_date.day, until_day: until_date.day, month: from_month, year: from_year, sep: separator)
       end
-
-      from_day = from_date.day
-      from_month = month_names[from_date.month]
-      from_year = from_date.year
-      until_day = until_date.day
-
-      if from_date.month == until_date.month
-        if from_date.day == until_date.day
-          I18n.l(from_date.to_date, :format => :long)
-        else
-          I18n.t("date_range.#{format}.same_month", from_day: from_date.day, until_day: until_date.day, month: from_month, year: from_year, sep: separator)
-        end
+    else
+      until_month = month_names[until_date.month]
+      if from_date.year == until_date.year
+        I18n.t("date_range.#{format}.different_months_same_year", from_day: from_date.day, from_month: from_month, until_day: until_date.day, until_month: until_month, year: from_year, sep: separator)
       else
-        until_month = month_names[until_date.month]
-        if from_date.year == until_date.year
-          I18n.t("date_range.#{format}.different_months_same_year", from_day: from_date.day, from_month: from_month, until_day: until_date.day, until_month: until_month, year: from_year, sep: separator)
+        until_year = until_date.year
+        I18n.t("date_range.#{format}.different_years", from_day: from_date.day, from_month: from_month, from_year: from_year, until_day: until_date.day, until_month: until_month, until_year: until_year, sep: separator)
+      end
+    end
+  end
+
+  def figure_width(resource, default = "small-12")
+    if resource.image.nil?
+      return default
+    else
+      if resource.image_width.nil?
+        return default
+      else
+        if resource.image_width > 1200
+          return default
+        elsif resource.image_width > 999
+          return "small-10"
+        elsif resource.image_width > 800
+          return "small-8"
         else
-          until_year = until_date.year
-          I18n.t("date_range.#{format}.different_years", from_day: from_date.day, from_month: from_month, from_year: from_year, until_day: until_date.day, until_month: until_month, until_year: until_year, sep: separator)
+          return "small-7"
         end
       end
     end
-
+  end
+  
   
   def find_nearest_space(string, start, counter = 18)
     return nil if string.nil?
