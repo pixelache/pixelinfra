c055eaa490138ca2b81783b80fc4d31eb2fa76c5
John W. Fail <fail@cenotaph.org>
2014-10-02 16:33:21 +0300

    new fixes

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,14 @@
+CKEDITOR.editorConfig = function( config ) {
+    config.language = 'en';
+    config.uiColor = '#AADC6E';
+    config.allowedContent = {
+        $1: {
+            // Use the ability to specify elements as an object.
+            elements: CKEDITOR.dtd,
+            attributes: true,
+            styles: true,
+            classes: true
+        }
+    };
+    config.disallowedContent = 'script; span; *[on*]; *{*}';
+};
