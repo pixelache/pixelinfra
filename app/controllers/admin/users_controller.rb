1eee5e40b919c4b8e71d5326a49ce1c6f9a63c38
John W. Fail <fail@cenotaph.org>
2014-11-10 14:38:57 +0200

    translation fixes and user sorting

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,30 @@
 class Admin::UsersController < Admin::BaseController
-
+  handles_sortable_columns
+  has_scope
+  
   def create
     create! { admin_users_path }
   end
   
   def update
     update! { admin_users_path }
+  end
+  
+  def index
+    order = sortable_column_order do |column, direction|
+      case column
+      when "name"
+        "LOWER(name) #{direction}"
+      when "email"
+        "email #{direction}"
+      when "registered"
+        "created_at #{direction}"
+      else
+        "created_at DESC"
+      end
+    end
+    @users = apply_scopes(User).includes(:roles).order(order).page(params[:page]).per(30)
+    set_meta_tags :title => 'Users'
   end
   
   protected
