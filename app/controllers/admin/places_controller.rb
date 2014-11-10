4de0ff2549cfdff89b58863fa044a5036df5900e
John W. Fail <fail@cenotaph.org>
2014-11-10 15:30:12 +0200

    place sorting

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -1,11 +1,28 @@
 class Admin::PlacesController < Admin::BaseController
-
+  handles_sortable_columns
+  
   def create
     create! { admin_places_path }
   end
   
   def update
     update! { admin_places_path }
+  end
+  
+  def index
+    order = sortable_column_order do |column, direction|
+      case column
+      when "name"
+        "LOWER(place_translations.name) #{direction}"
+      when "address"
+        "address #{direction}, start_at #{direction}"
+
+      else
+        "LOWER(place_translations.name)"
+      end
+    end
+    @places = Place.joins(:translations).order(order).page(params[:page]).per(50)
+    set_meta_tags :title => t(:places)
   end
   
   def set_fi
