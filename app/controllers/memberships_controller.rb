a11947b8fe472d43e9a8a2b570d82fb5ca08209c
John W. Fail <fail@cenotaph.org>
2015-01-24 15:07:24 +0200

    display past members

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,6 +3,7 @@ class MembershipsController < ApplicationController
   def index
     years = Membership.pluck(:year).sort
     @members = Membership.joins(:user).where(year: years.last)
+    @past_members = Membership.all.to_a.uniq(&:user).delete_if{|x| @members.map(&:user).include? x.user }
     set_meta_tags title: t(:members)
   end
   
