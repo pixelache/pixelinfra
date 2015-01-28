511a01bfaef5cca767d983d1e057d69a66c4855e
John W. Fail <fail@cenotaph.org>
2015-01-28 22:44:44 +0200

    updated globalize

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -14,13 +14,18 @@ class Admin::PagesController < Admin::BaseController
   autocomplete :page, :name, :extra_data => [:name], :display_value => :name
   
   def create
-    create! { admin_pages_path }
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:notice] = 'Page saved.'
+      redirect_to admin_pages_path
+    end
   end
   
   
   def destroy
     @page = Subsite.find(params[:subsite_id]).pages.find(params[:id])
-    destroy! { admin_pages_path }
+    @page.destroy!
+    redirect_to admin_pages_path
   end
   
   def edit
@@ -81,8 +86,8 @@ class Admin::PagesController < Admin::BaseController
   
   protected
   
-  def permitted_params
-    params.permit(:page => [:published, :slug, :festival_id, :parent_id, :festival_name, :project_name, :project_id, :subsite_id, subsite_ids: [], translations_attributes: [:id, :locale, :name, :body],       festivaltheme_ids: []] )
+  def page_params
+    params.require(:page).permit( [:published, :slug, :festival_id, :parent_id, :festival_name, :project_name, :project_id, :subsite_id, subsite_ids: [], translations_attributes: [:id, :locale, :name, :body],       festivaltheme_ids: []] )
   end
   
 end
