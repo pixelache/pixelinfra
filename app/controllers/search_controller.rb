d02ecab2dab55f5a094366a1b597975669a7bc44
John W. Fail <fail@cenotaph.org>
2015-02-07 14:24:05 +0200

    Ags's design for OSF brought current

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,16 @@
 class SearchController < ApplicationController
   
   def create
+ 
     @hits = []
     @term = params[:search][:term]
-    @hits += Event.with_translations.advanced_search({event_translations: {name: @term}, event_translations: {description: @term}}, false)
-    @hits += Post.published.with_translations.advanced_search({post_translations: {title: @term}, post_translations: {body: @term}}, false)
-    @hits += Project.with_translations.advanced_search({name: @term, project_translations: {description: @term}}, false)
-    @hits += Residency.with_translations.advanced_search(residency_translations: {description: @term })
-    @hits += Page.published.with_translations.advanced_search( { :page_translations => { name: @term}, :page_translations => {body: @term}}, false )
+    @hits += Event.by_site(@site).with_translations.advanced_search({event_translations: {name: @term}, event_translations: {description: @term}}, false)
+    @hits += Post.by_site(@site).published.with_translations.advanced_search({post_translations: {title: @term}, post_translations: {body: @term}}, false)
+    if @site.id == 1
+      @hits += Project.with_translations.advanced_search({name: @term, project_translations: {description: @term}}, false)
+      @hits += Residency.with_translations.advanced_search(residency_translations: {description: @term })
+    end
+    @hits += Page.by_site(@site).published.with_translations.advanced_search( { :page_translations => { name: @term}, :page_translations => {body: @term}}, false )
     @hits.flatten!
     @hits.uniq!
     set_meta_tags title: t(:search_results_for, result: @term)
