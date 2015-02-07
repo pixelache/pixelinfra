a401b47eaff073e53c9b443704f776d8439d77ac
John W. Fail <fail@cenotaph.org>
2015-02-07 20:51:32 +0200

    steps design

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,6 +1,8 @@
 class PublicationsController < ApplicationController
   
   def index
+    @interviews = Post.by_site(@site).map(&:attachments).flatten.compact
+    @videos = Event.by_site(@site).map(&:videos).flatten.compact
   end
   
 end
