ff7dbd6b18cb410192f6fa1cb3612b9e91870a3c
John W. Fail <fail@cenotaph.org>
2015-02-07 17:14:37 +0200

    force admin mode into english

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,18 @@
 class Admin::BaseController < InheritedResources::Base
   layout 'admin'
-
+  before_filter :force_english
+  
   before_filter :authenticate_user!
   #load_and_authorize_resource
   # check_authorization
   load_and_authorize_resource :find_by => :slug
   skip_before_filter :require_no_authentication
   before_filter :set_meta_tagz
+  
+  
+  def force_english
+    I18n.locale = 'en'
+  end
   
   def set_meta_tagz
     set_meta_tags :title => params[:controller].gsub(/admin\//, '').humanize
