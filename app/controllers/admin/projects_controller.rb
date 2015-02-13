8b75b539b6ef12ec9cc1cf762d9cc8f79e50fb08
John W. Fail <fail@cenotaph.org>
2015-02-13 16:35:19 +0200

    projects control panel sorta exists

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -27,7 +27,7 @@ class Admin::ProjectsController < Admin::BaseController
   protected
   
   def permitted_params
-    params.permit(:project => [:name, :slug, :parent_id, :website, :evolvedfrom_id, :project_bg_colour, :project_text_colour, :project_link_colour, :redirect_to, :background, :remove_background, :evolution_year, :has_listserv, :listservname, :website, :active, translations_attributes: [:description, :short_description, :id, :locale], photos_attributes: [:id, :filename, :_destroy], videos_attributes: [:id, :in_url, :_destroy], attachments_attributes: [:id, :attachedfile, :_destroy]])
+    params.permit(:project => [:name, :slug, :parent_id, :website, :evolvedfrom_id, :project_bg_colour, :project_text_colour, :project_link_colour, :redirect_to, :background, :remove_background, :evolution_year, :has_listserv, :listservname, :website, :active, translations_attributes: [:description, :short_description, :id, :locale], photos_attributes: [:id, :filename, :_destroy], videos_attributes: [:id, :in_url, :_destroy], attachments_attributes: [:id, :attachedfile,:title, :description, :public, :item_type, :item_id, :documenttype_id,  :_destroy]])
   end
     
 end 
