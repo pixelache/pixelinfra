5e9a645abe17af125b5babf8ed6ad375a49ef6ef
John W. Fail <fail@cenotaph.org>
2015-02-17 10:50:27 +0200

    countries fix

--- app/controllers/residencies_controller.rb
+++ app/controllers/residencies_controller.rb
@@ -2,8 +2,8 @@ class ResidenciesController < ApplicationController
   
   def index
     @page = @site.pages.published.find('residencies')
-    @microresidencies = Residency.micro
-    @production = Residency.production
+    @microresidencies = Residency.micro.order('end_at DESC')
+    @production = Residency.production.order('end_at DESC')
     
     set_meta_tags :title => t(:residencies)
   end
@@ -11,8 +11,8 @@ class ResidenciesController < ApplicationController
   
   def show
     @residency = Residency.find(params[:id])
-    @microresidencies = Residency.micro
-    @production = Residency.production
+    @microresidencies = Residency.micro.order('end_at DESC')
+    @production = Residency.production.order('end_at DESC')
     
     set_meta_tags :title => t(:residencies) + " - " + @residency.name
   end
