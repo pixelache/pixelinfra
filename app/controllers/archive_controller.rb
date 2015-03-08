0f579cd61c534a4fb1c873c595f15b91d5ded476
John W. Fail <fail@cenotaph.org>
2015-03-08 16:46:15 +0200

    archive in progress

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -3,7 +3,11 @@ class ArchiveController < ApplicationController
   def show
     @events = Event.by_subsite(@site).by_year(params[:id]).where(:festival_id => nil)
     @festival = Festival.by_node(Node.find_by(:name => 'Pixelache Helsinki')).by_year(params[:id]).first
+    @projects = @events.map(&:project).compact.uniq
+    
     @posts = Post.by_subsite(@site).published.by_year(params[:id])
+    @projects += @posts.map(&:project).compact.uniq
+    @projects = @projects.flatten.uniq.sort_by{|x| x.slug }
     @year = params[:id]
     set_meta_tags :title => t(:archive) + " " + @year
   end
