a8979448dbfa9b6311391799c664527691908b1b
John W. Fail <fail@cenotaph.org>
2015-03-10 22:31:23 +0200

    archival page fixes

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -6,10 +6,10 @@ class ArchiveController < ApplicationController
     @festival = Festival.by_node(Node.find_by(:name => 'Pixelache Helsinki')).by_year(params[:id]).first
     @projects = @events.map(&:project).compact.uniq
     @residencies = Residency.where(["(start_at >= ? AND start_at <= ?) OR (end_at >= ? AND end_at <= ?)",  "#{@year}-01-01", "#{@year}-12-31", "#{@year}-01-01", "#{@year}-12-31" ])
-    @posts = Post.by_subsite(@site).published.by_year(params[:id])
+    @posts = Post.by_subsite(@site).published.by_year(params[:id]).order(:published_at)
     @projects += @posts.map(&:project).compact.uniq
     @projects = @projects.flatten.uniq.sort_by{|x| x.slug }
-    
+    @events = Event.by_subsite(@site).by_year(params[:id]).published.where(:festival_id => nil).order(:start_at)
     @documents = Document.joins(:attachment).where(["date_of_release >= ? AND date_of_release <= ?", "#{@year}-01-01", "#{@year}-12-31"]).where("attachments.public = true")
     @photos = Photo.by_year(params[:id]).to_a.delete_if{|x| x.item.class == Post }.delete_if{|x| x.item.subsite != @site }
     @photos += Archivalimage.by_site(@site).by_year(params[:id])
