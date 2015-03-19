a336128cf75d686b6fe9e99c95613f9220119f0e
John W. Fail <fail@cenotaph.org>
2015-03-19 15:28:34 +0200

    search algorithms are now the same. Fixes #9

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -2,11 +2,20 @@ class Admin::SearchController < ApplicationController
   layout 'admin'
   
   def create
+ 
     @hits = []
-    @hits += Post.with_translations.advanced_search(params[:searchterm])
-    @hits += Page.with_translations.advanced_search(params[:searchterm])
-    @hits += Event.with_translations.advanced_search(params[:searchterm])
-    @hits += Project.with_translations.advanced_search(params[:searchterm])
+    @term = params[:searchterm]
+    @hits += Event.by_site(@site).with_translations.advanced_search({event_translations: {name: @term}, event_translations: {description: @term}}, false)
+    @hits += Post.by_site(@site).published.with_translations.advanced_search({post_translations: {title: @term}, post_translations: {body: @term}}, false)
+    if @site.id == 1
+      @hits += Project.with_translations.advanced_search({name: @term, project_translations: {description: @term}}, false)
+      @hits += Residency.with_translations.advanced_search(residency_translations: {description: @term })
+    end
+    @hits += Page.by_site(@site).published.with_translations.advanced_search( { :page_translations => { name: @term}, :page_translations => {body: @term}}, false )
+    @hits.flatten!
+    @hits.uniq!
+    # @hits = @hits.group_by{|x| x.class.to_s }
+    set_meta_tags title: t(:search_results_for, result: @term)
   end
   
 end
