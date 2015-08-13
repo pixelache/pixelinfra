2db85c6fee89f5d3e4281931f770eb93792d9f01
John W. Fail <john@pixelache.ac>
2015-08-13 14:14:59 +0300

    locations map fix

--- app/controllers/admin/frontitems_controller.rb
+++ app/controllers/admin/frontitems_controller.rb
@@ -5,10 +5,14 @@ class Admin::FrontitemsController < Admin::BaseController
   autocomplete :project, :name, :extra_data => [:name], :display_value => :name
   autocomplete :festival, :name, :extra_data => [:name], :display_value => :name
   autocomplete :residency, :name, :extra_data => [:name], :display_value => :name
-  has_scope :by_site , default: 1
+  # has_scope :by_site , default: 1
   
   def create
     create! { admin_frontitems_path }
+  end
+  
+  def edit
+    @frontitems = Frontitem.find(params[:id])
   end
   
   def index
