3fd4961dab80a3ac54c303f92923703a7c87af8b
John W. Fail <john@pixelache.ac>
2015-08-14 13:01:53 +0300

    edit inside*out proposals

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,7 +1,7 @@
 class Attendee < ActiveRecord::Base
   belongs_to :user
   belongs_to :item, polymorphic: true
-
+  validates_presence_of :email
   
   scope :by_festival, -> (festival) { where(:item_id => festival, :item_type => "Festival")}
   scope :by_event, -> (event) { where(:item_id => event, :item_type => "Event")}
