1930682a5c1d65252a56555b430bec972db8563f
John W. Fail <john@pixelache.ac>
2016-12-07 15:16:16 +0200

    projects

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -77,15 +77,15 @@ class Admin::ProjectsController < Admin::BaseController
   
   protected
   
-  def permitted_params
-    params.permit(:project => [:name, :slug, :parent_id, :website, :evolvedfrom_id, :hidden, :project_bg_colour,
+  def project_params
+    params.require(:project).permit(:name, :slug, :parent_id, :website, :evolvedfrom_id, :hidden, :project_bg_colour,
        :project_text_colour, :project_link_colour, :redirect_to, :background, :remove_background,
         :evolution_year, :has_listserv, :listservname, :website, :active, 
         translations_attributes: [:description, :short_description, :id, :locale], 
         photos_attributes: [:id, :filename, :_destroy], 
         videos_attributes: [:id, :in_url, :_destroy], 
         attachments_attributes: [:id, :year_of_publication, :attachedfile,:title, :description, :public, :item_type, 
-          :item_id, :documenttype_id,  :_destroy]])
+          :item_id, :documenttype_id,  :_destroy])
   end
     
 end 
