8b9b10b543203f81926cd94b0d31410a2af8b82e
John W. Fail <john@pixelache.ac>
2016-12-16 11:15:43 +0200

    new video code

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,12 +2,12 @@ module VideosHelper
   
   def figure_video_type(video, width = 800, height = 600)
     if video.video_provider == 'youtube' || video.url =~ /youtube\.com/
-      
-      return auto_html(video.url) { youtube(:width => width, :height => height)}
+      return '<iframe id="ytplayer" type="text/html" width="' + video.width.to_s + '" height="' + video.height.to_s + '"
+  src="https://www.youtube.com/embed/' + video.hostid.to_s + '" frameborder="0"></iframe>'
+
     elsif video.video_provider == 'vimeo'|| video.url =~ /vimeo\.com/
       
-      # return '<iframe src="//player.vimeo.com/video/VIDEO_ID" width="' + vimdeo" height="HEIGHT" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-      return auto_html(video.url) { vimeo(:width => width, :height => height)}
+      return '<iframe src="//player.vimeo.com/video/' + video.hostid.to_s + '" width="' + video.video_width.to_s + '" height="' + video.video_height.to_s + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     end
   end
   
