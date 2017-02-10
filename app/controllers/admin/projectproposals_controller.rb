89ebe40a6d8760485b1cf3940767c2c9677ca8f5
John W. Fail <john@pixelache.ac>
2017-02-10 12:02:42 +0200

    project proposals can be created and edited again

--- app/controllers/admin/projectproposals_controller.rb
+++ app/controllers/admin/projectproposals_controller.rb
@@ -1,9 +1,33 @@
 class Admin::ProjectproposalsController < Admin::BaseController
   handles_sortable_columns
   
+  
+  def create
+    @projectproposal = Projectproposal.new(projectproposal_params)
+    if @projectproposal.save
+      flash[:notice] = 'Project proposal saved.'
+      redirect_to admin_projectproposals_path
+    end
+  end
+  
   def show
     @projectproposal = Projectproposal.find(params[:id])
     set_meta_tags :title => @projectproposal.name
+  end
+  
+  def edit
+    @projectproposal = Projectproposal.friendly.find(params[:id])
+    set_meta_tags :title => 'Edit: ' + @projectproposal.name
+  end
+  
+  def update
+    @projectproposal = Projectproposal.friendly.find(params[:id])
+    if @projectproposal.update_attributes(projectproposal_params)
+      flash[:notice] = 'Proposal updated.'
+      redirect_to admin_projectproposals_path
+    else
+      flash[:error] = 'Error updating page: ' + @page.errors.inspect
+    end
   end
   
   def index
@@ -28,14 +52,15 @@ class Admin::ProjectproposalsController < Admin::BaseController
   
   
   def new
+    @projectproposal = Projectproposal.new
     set_meta_tags :title => t(:new_project_proposal)
-    super
+    
   end
   
   protected
   
-  def permitted_params
-    params.permit(:projectproposal => [:name, :project_id, :archived, :festival_id, :event_id, :description, :long_description, :primary_initiator_id, :cosupporters, :producer, :treasurer, :documentation, :communication, :additional_experts, :reporting, :imagined_participants, :equipment, :offspring_id, :budget, :external_funding, :inkind, :people_expertise, :where, :when, :when_will_it_end, :slug, :why])
+  def projectproposal_params
+    params.require(:projectproposal).permit(:name, :project_id, :archived, :festival_id, :event_id, :description, :long_description, :primary_initiator_id, :cosupporters, :producer, :treasurer, :documentation, :communication, :additional_experts, :reporting, :imagined_participants, :equipment, :offspring_id, :budget, :external_funding, :inkind, :people_expertise, :where, :when, :when_will_it_end, :slug, :why)
   end
 
 end
