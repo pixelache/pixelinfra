32d75008151b6a4f18856d410dbffe1990f9fba6
John W. Fail <john@pixelache.ac>
2017-05-21 16:57:19 +0300

    start of 2017 festival

---
+++ app/assets/themes/festival2017/javascripts/application.js
@@ -0,0 +1,12 @@
+//= require jquery2
+//= require jquery_ujs
+//= require jquery-ui/core
+//= require foundation
+//= require jquery.infinite-pages
+
+
+ $( function() {
+
+   $(document).foundation();
+
+});
