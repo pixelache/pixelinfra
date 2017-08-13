7f3c64e97e08bf83dc1177c8c2a59bbf34c530c8
John W. Fail <john@pixelache.ac>
2017-08-13 17:01:57 +0300

    favicon and programme js

--- app/assets/themes/festival2017/javascripts/application.js
+++ app/assets/themes/festival2017/javascripts/application.js
@@ -10,3 +10,13 @@
    $(document).foundation();
 
 });
+
+function toggleEvent(d) {
+  $('.event_link' + d).fadeIn();
+  $('.event_link').not(d).fadeOut();
+  $('.filters ul li').removeClass('active');
+  if ($('.event_link' + d).css('display') != "none") {
+
+    $('.filters ul li' + d).addClass('active');
+  }
+}
