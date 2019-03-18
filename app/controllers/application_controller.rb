1b829608f9aa22083a3a2cf8de397a6d8f24c02b
John W. Fail <john@pixelache.ac>
2019-03-19 00:44:33 +0200

    link festival url to new site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -162,8 +162,8 @@ class ApplicationController < ActionController::Base
         @site = Subsite.find_by(name: 'documenting')
       elsif request.host =~ /^empathy/ 
         @site = Subsite.find_by(name: 'empathy') 
-      elsif   request.host =~ /^festival2017/    || request.host =~ /^festival/
-         @site = Subsite.find_by(name: 'festival2017') 
+      elsif   request.host =~ /^festival2017/   
+         @site = Subsite.find_by(name: 'festival2017')
       else
         @site = Subsite.find_by(:name => (request.host =~ /opensourcingfestivals/ || request.host =~ /^olsof\./ ? 'olsof' : 'pixelache'))
       end
