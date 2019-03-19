fcb3402f39432dff89b22150ba216394bad9c9a3
John W. Fail <john@pixelache.ac>
2019-03-19 15:13:50 +0200

    force XHR on dynamic taglines

--- app/controllers/dynamictaglines_controller.rb
+++ app/controllers/dynamictaglines_controller.rb
@@ -1,8 +1,8 @@
 class DynamictaglinesController < ApplicationController
-  
   def index
     @dynamictagline = Dynamictagline.find_by(:subsite_id => @site.id).tagline rescue "Festival of Electronic Art and Subcultures"
-    
+    unless request.xhr?
+      redirect_to '/'
+    end
   end
-  
 end
