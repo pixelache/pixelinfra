470c47f22486fa0d7831733d8b45ee4660f43624
John W. Fail <john@pixelache.ac>
2019-03-20 18:55:08 +0200

    false layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :render_404 unless Rails.env.development?
 
   def render_404(exception)
-    render file: "#{Rails.root}/public/404.html", status: 404
+    render file: "#{Rails.root}/public/404.html", status: 404, layout: false
   end
 
   def render_500(exception)
