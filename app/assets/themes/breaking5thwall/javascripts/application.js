3ca4673bf21d40afd8d789bae2b2db0170090c96
John W. Fail <john@pixelache.ac>
2019-04-13 12:11:18 +0300

    news show and mobile responsiveness

--- app/assets/themes/breaking5thwall/javascripts/application.js
+++ app/assets/themes/breaking5thwall/javascripts/application.js
@@ -1,3 +1,15 @@
 //= require jquery2
 //= require jquery_ujs
 //= require jquery-ui/core
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
