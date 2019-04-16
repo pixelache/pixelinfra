d09fe2242f2a9fc78d494d895d217922f2b5cf08
John W. Fail <john@pixelache.ac>
2019-04-16 11:44:30 +0300

    prevent destroying places

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,8 +5,10 @@ class Place < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, :on => :create
   accepts_nested_attributes_for :translations, :reject_if => proc {|x| x['name'].blank? }
-  has_many :events, :dependent => :nullify
+  has_many :events, :dependent => :restrict_with_error
   
+  # before_destroy :check_for_events
+
   def name_en
     self.name(:en)
   end
@@ -18,4 +20,15 @@ class Place < ActiveRecord::Base
   def address_no_country
     [address1 , address2, city].delete_if {|x| x.blank? }.compact.join(', ')
   end
+
+  private
+
+  def check_for_events
+    if events.published.count > 0
+      errors.add_to_base('You cannot delete this place because events in the past have taken place there.')
+      return false
+    end
+  end
+
+
 end
