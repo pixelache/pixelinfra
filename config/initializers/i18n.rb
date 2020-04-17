1285ecd6ef81aa4bb612dcdb8107bd855e843ad3
John W. Fail <john@pixelache.ac>
2020-04-17 13:16:41 +0300

    swedish and russian support

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,8 +1,10 @@
 I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 I18n.default_locale = :en
-I18n.available_locales = [:en,  :fi]
-I18n.fallbacks[:en] = [:en,  :fi]
-I18n.fallbacks[:fi] = [:fi,  :en]
+I18n.available_locales = [:en, :fi, :sv, :ru]
+I18n.fallbacks[:en] = [:en, :fi, :sv, :ru]
+I18n.fallbacks[:fi] = [:fi, :en, :sv, :ru]
+I18n.fallbacks[:sv] = [:sv, :en, :fi, :ru]
+I18n.fallbacks[:ru] = [:ru, :en, :sv, :fi]
 
-Globalize.fallbacks = {:en => [:en, :fi], :fi => [:fi, :en] }
+Globalize.fallbacks = { en: [:en, :fi, :sv, :ru], fi: [:fi, :sv, :en, :ru], sv: [:sv, :en, :fi, :ru], ru: [:ru, :en, :sv, :fi] }
 I18n.enforce_available_locales = false
