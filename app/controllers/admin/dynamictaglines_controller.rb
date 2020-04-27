5d2560c998c5cc5e2a342d5196a66d9a15ae807c
John W. Fail <john@pixelache.ac>
2020-04-27 14:35:36 +0300

    dynamic tagline fix

--- app/controllers/admin/dynamictaglines_controller.rb
+++ app/controllers/admin/dynamictaglines_controller.rb
@@ -1,11 +1,17 @@
 class Admin::DynamictaglinesController < Admin::BaseController
-  
+  skip_load_and_authorize_resource
+  load_and_authorize_resource
+
   def create
     create! { admin_dynamictaglines_path }
   end
   
   def update
-    update! { admin_dynamictaglines_path}
+    @dynamictagline = Dynamictagline.find(params[:id])
+    if @dynamictagline.update(dynamictagline_params)
+      flash[:notice] = 'Tagline settings updated'
+      redirect_to admin_dynamictaglines_path
+    end
   end
   
   def index
@@ -14,8 +20,8 @@ class Admin::DynamictaglinesController < Admin::BaseController
   
   protected
   
-  def permitted_params
-    params.permit(:dynamictagline => [:festival, :electronic, :art, :subcultures])
+  def dynamictagline_params
+    params.require(:dynamictagline).permit(:festival, :electronic, :art, :subcultures)
   end
 end
   
