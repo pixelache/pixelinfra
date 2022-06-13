be0fe0528dbdeacb9d837ef8b664aedf495936ab
John W. Fail <john@ptarmigan.fi>
2022-06-13 18:03:59 +0300

    CSS adjustments and header tags permitlisted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -206,7 +206,7 @@ module ApplicationHelper
   def pixelache_format(text, html_options = {}, options = {})
     text = '' if text.nil?
     text = smart_truncate(text, options[:truncate]) if options[:truncate].present?
-    text = sanitize(text, tags: %w{a bold img iframe italic b i em br p}, attr: %w{href src width align height}) unless options[:sanitize] == false
+    text = sanitize(text, tags: %w{a bold img iframe italic h1 h2 h3 h4 h5 h6 b i em br p}, attr: %w{href src width align height}) unless options[:sanitize] == false
     text = text.to_str
     text.gsub!(/\r\n?/, "\n") # \r\n and \r -> \n
     text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline -> br
