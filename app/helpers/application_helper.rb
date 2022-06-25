3e34bfac5d7427a2a2a88609e20f7840534088e5
John W. Fail <john@ptarmigan.fi>
2022-06-25 17:00:30 +0300

    Mobile adjustments for pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -206,7 +206,7 @@ module ApplicationHelper
   def pixelache_format(text, html_options = {}, options = {})
     text = '' if text.nil?
     text = smart_truncate(text, options[:truncate]) if options[:truncate].present?
-    text = sanitize(text, tags: %w{a bold img b strong iframe italic h1 h2 h3 h4 h5 h6 b i em br p}, attr: %w{href src width align height}) unless options[:sanitize] == false
+    text = sanitize(text, tags: %w{a bold img b div strong iframe italic h1 h2 h3 h4 h5 h6 b i em br p}, attr: %w{href src width class id align height}) unless options[:sanitize] == false
     text = text.to_str
     text.gsub!(/\r\n?/, "\n") # \r\n and \r -> \n
     text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline -> br
